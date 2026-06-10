ab8f2ac281a77a5a190011e89aa71edfb70c53f2
daniellaszlo123 <daniellaszlo002@gmail.com>
2026-06-10 22:13:57 +0200

    feat: Add Capacitor and native Android wrapper project

---
+++ android/app/src/main/java/com/daniellaszlo/studycard/MainActivity.java
@@ -0,0 +1,5 @@
+package com.daniellaszlo.studycard;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.daniellaszlo.studycard',
+  appName: 'StudyCard',
+  webDir: 'dist'
+};
+
+export default config;
